76684388dbd6325ad3518746d7436b5bac352c83
Anish Korade <161752586+AnishWerkstatt@users.noreply.github.com>
2026-06-15 01:21:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.02%) - LeetSync

---
+++ 274-h-index/h-index.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        pc = [0] * (n+1)
+
+        for c in citations:
+            pc[min(n,c)] += 1
+
+        h = n
+        papers = pc[n]
+
+        while papers < h:
+            h -= 1
+            papers += pc[h]
+        
+        return h
+
+        
