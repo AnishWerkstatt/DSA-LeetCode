5408fc3687b34ed1cdeb1516aa270a59d8c28a60
Anish Korade <161752586+AnishWerkstatt@users.noreply.github.com>
2026-06-16 23:49:17 +0530

    Time: 2 ms (8.13%) | Memory: 19.1 MB (90.10%) - LeetSync

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        temp = n
+        sm = 0
+        prod = 1
+        while temp > 0:
+            r = temp % 10
+            prod *= r
+            sm += r
+            temp //= 10
+        ans = prod - sm
+        return ans
+
+        
